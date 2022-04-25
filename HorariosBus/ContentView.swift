a7655e5aed321c9ea27276036edeb64ad34b6f39
EsAlco <90135660+EsAlco@users.noreply.github.com>
2022-04-25 11:33:13 +0200

    Initial Commit

---
+++ HorariosBus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HorariosBus
+//
+//  Created by Esther Alcoceba Gutiérrez de León on 25/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HorariosBus/HorariosBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorariosBusApp.swift
+//  HorariosBus
+//
+//  Created by Esther Alcoceba Gutiérrez de León on 25/4/22.
+//
+
+import SwiftUI
+
+@main
+struct HorariosBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
