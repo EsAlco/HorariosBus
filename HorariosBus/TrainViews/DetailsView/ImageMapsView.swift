04763d49de245326abbe8e2ebc01800241bfa8fc
EsAlco <90135660+EsAlco@users.noreply.github.com>
2022-07-11 00:22:10 +0200

    Implementación del DragGesture en la vista del mapa de cercanías.

--- HorariosBus/TrainViews/DetailsView/ImageMapsView.swift
+++ HorariosBus/TrainViews/DetailsView/ImageMapsView.swift
@@ -9,20 +9,34 @@ import SwiftUI
 
 struct ImageMapsView: View {
     
-    @State var scale: CGFloat = 1.0
+    @State private var scale: CGFloat = 1.0
+    @State private var position = CGSize.zero
+    
+    @GestureState private var offset = CGSize.zero
     
     var body: some View {
         NavigationView{
             Image("CercaniasMadrid")
                 .resizable()
-                .scaledToFit()
+                .scaledToFill()
                 .padding()
                 .scaleEffect(scale)
+                .offset(
+                    x: position.width + offset.width,
+                    y: position.height + offset.height)
                 .gesture(
                     MagnificationGesture()
-                        .onChanged ({ value in
-                            scale = value
-                        })
+                        .onChanged{ value in
+                            self.scale = value.magnitude
+                        }
+                        .simultaneously(with: DragGesture())
+                        .updating($offset){ (value, state, transaction) in
+                            state = value.second?.translation ?? .zero
+                        }
+                        .onEnded{ (value) in
+                            self.position.height += value.second?.translation.height ?? .zero
+                            self.position.width += value.second?.translation.width ?? .zero
+                        }
                 )
             
         }
